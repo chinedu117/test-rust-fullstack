5d13589b40e8121265138eee8c68056dcb58a9de
fcruxen <fcruxen@gmail.com>
2023-03-18 10:05:51 -0300

    refactored

--- ui/src/components/crud/list.rs
+++ ui/src/components/crud/list.rs
@@ -1,9 +1,10 @@
+use std::{rc::Rc};
 use bevy_reflect::{Struct};
 use serde::{Deserialize, Serialize};
 use yew::{prelude::*};
 use yew_bootstrap::component::{Line};
 use gloo_net::{http::{Method::GET}};
-use crate::{api::Client, components::common::error::{ErrorToast}, UserContext};
+use crate::{api::Client, UserContext, ErrorContext, stores::error::ErrorState};
 use super::table::CrudTable;
 use gloo_console::log;
 
@@ -20,13 +21,10 @@ pub fn list<M>(props: &ItensProps) -> Html where
         let all: UseStateHandle<Vec<M>> = use_state(|| vec![]);
         let all_clone = all.clone();
         let set_all = Callback::from(move |list| all_clone.set(list));        
-        let request_error = use_state(|| "".to_string());
-        let request_error_clone = request_error.clone();
-        let set_error = Callback::from(move |request_error| request_error_clone.set(request_error)); 
-        let user_context = use_context::<UserContext>().expect("no ctx found for UserContext");             
+        let user_context = use_context::<UserContext>().expect("no ctx found for UserContext");
+        let error_context = use_context::<ErrorContext>().expect("no ctx found for UserContext");             
         {    
-            let set_all = set_all.clone();            
-            let set_error = set_error.clone();                            
+            let set_all = set_all.clone();                                                    
             let props = props.clone();                        
             use_effect_with_deps(move |_| {                
                 let props = props.clone();                
@@ -42,8 +40,13 @@ pub fn list<M>(props: &ItensProps) -> Html where
                                 .unwrap();
                             set_all.emit(fetched)
                         }
-                        Err(err) => {                            
-                            set_error.emit(err.to_string());                            
+                        Err(err) => {                               
+                            let s: Callback<ErrorState> = error_context.dispatch.reduce_callback(move |_| {
+                                let state = ErrorState { error: Some(err.to_string()) };
+                                Rc::new(state)
+                            });
+                            s.emit(Default::default())
+
                         }
                     }    
                 });
@@ -55,8 +58,7 @@ pub fn list<M>(props: &ItensProps) -> Html where
                 <Line />
                 <h3>{props.title.clone()}</h3>
                 <Line />
-                <table class={"table table-dark"}><CrudTable<M> list={all.clone().to_vec()} /></table>
-                <ErrorToast error={request_error.clone().to_string()} />   
+                <CrudTable<M> list={all.clone().to_vec()} />                
             </>
         }
 }
--- ui/src/main.rs
+++ ui/src/main.rs
@@ -6,9 +6,11 @@ use yew_router::prelude::*;
 use yew_bootstrap::util::*;
 use yew_bootstrap::component::{Row, Column};
 use yewdux::prelude::{use_store, Dispatch};
+use crate::components::common::error_toast::ErrorToast;
 use crate::components::common::main_menu::MainMenu;
 use crate::routes::{Route, switch};
-use crate::stores::user::UserState;
+use crate::stores::user::{UserState};
+use crate::stores::error::{ErrorState};
 use yew::{function_component, html, Html, ContextProvider, Properties};
 
 #[derive(Clone, Default, Properties, PartialEq)]
@@ -17,28 +19,40 @@ pub struct UserContext {
     dispatch: Dispatch<UserState>
 }
 
+#[derive(Clone, Default, Properties, PartialEq)]
+pub struct ErrorContext {
+    state: ErrorState,
+    dispatch: Dispatch<ErrorState>
+}
+
 
 #[function_component(App)]
 fn app() -> Html {
-    let (state, dispatch) = use_store::<UserState>();
-    let user_context = UserContext {state: (*state).clone(), dispatch: dispatch.clone()};    
+    let (user_state, user_dispatch) = use_store::<UserState>();
+    let (error_state, error_dispatch) = use_store::<ErrorState>();
+    
+    let user_context = UserContext {state: (*user_state).clone(), dispatch: user_dispatch.clone()};    
+    let error_context = ErrorContext {state: (*error_state).clone(), dispatch: error_dispatch.clone()};    
     html! {
         <>  
             {include_inline()}
             {include_cdn_icons()}
             <ContextProvider<UserContext> context={user_context}>
-                <BrowserRouter>
-                    <Row>
-                    <Column class="bg-primary">
-                        <MainMenu />
-                    </Column>
-                    </Row>
-                    <Row>
-                    <Column>
-                        <Switch<Route> render={switch} /> 
-                    </Column>
-                    </Row>
-                </BrowserRouter>
+                <ContextProvider<ErrorContext> context={error_context}>
+                    <BrowserRouter>
+                        <Row>
+                        <Column class="bg-primary">
+                            <MainMenu />                            
+                        </Column>
+                        </Row>
+                        <Row>
+                        <Column>
+                            <ErrorToast />
+                            <Switch<Route> render={switch} />                             
+                        </Column>
+                        </Row>
+                    </BrowserRouter>
+                </ContextProvider<ErrorContext>>
             </ContextProvider<UserContext>>
             { include_cdn_js() }
         </>
